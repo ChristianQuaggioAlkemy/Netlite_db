7d3cfdf4d252c7fabb707ff973b7626552cf6bc7
ChristianQuaggioAlkemy <125556784+ChristianQuaggioAlkemy@users.noreply.github.com>
2023-03-27 17:52:57 +0200

    Delete try.sh

--- try.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ./.env
-
-rp=$(readlink -f "./indexes.sql")
-echo ${rp}
-psql -h $PGHOST -U $PGUSER -d $PGDATABASE -f ${rp}
-rp=$(readlink -f "./partitions.sql")
-psql -h $PGHOST -U $PGUSER -d $PGDATABASE -f ${rp}
